e011ad16ab842b0ffb520c35121e16c12f1b9a2a
armanpinachyan <pinachyan99@mail.ru>
2022-11-05 00:39:01 +0400

    the SHOP

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
 
   return (
     <div className='Footer' >
-      <h5 class="footertext"> crated by <span>Hayk Abelyan | </span>All rights reserved</h5>
+      <h5 class="footertext"> crated by <span>Arman Pinachyan | </span>All rights reserved</h5>
       <div className="icons">
      <a href='https://www.facebook.com/hayk.abelyan.3' target="_blank" > <i class="fa-brands fa-facebook"></i></a>
      <a href='https://www.instagram.com/hayk.abelyan.3/' target="_blank" ><i class="fa-brands fa-instagram"></i></a>
